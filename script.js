c4fdd6d22b8a752bb67aea9c0207085dd5a8f7a2
SebastianLomme <77025210+SebastianLomme@users.noreply.github.com>
2021-08-05 14:45:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const paintWalls = function(color){
+    console.log(`The wall has been painted ${color}`)
+} 
+
+paintWalls("red")
+paintWalls("Blue")
+
+const paintWallDirection = function(color, direction) {
+    console.log(`The ${direction} wall has been painted ${color}`)
+}
+
+paintWallDirection("red", "noth")
